6c9ce9186f9a04f663304d95b0cce53c99ab7a67
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-08-23 12:39:30 +0300

    Implement WebSocket server and refactor Express app to use it; streamline middleware setup and improve user connection handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,9 +5,14 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from "./lib/socket";
 
-const app = express();
+const PORT = process.env.PORT || 5001;
 
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -15,18 +20,10 @@ app.use(
   })
 );
 
-dotenv.config();
-
-const PORT = process.env.PORT || 5001;
-
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use(cookieParser());
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("running on ", PORT);
   connectDB();
 });
